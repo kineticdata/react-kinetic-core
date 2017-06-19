7bbedd35aa1e58629ba006f6ed1a7764bbe1d1f0
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-06-19 14:01:16 -0500

    updated CoreForm to properly handle componentDidUpdate

--- src/components/CoreForm.js
+++ src/components/CoreForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { bundle, K } from '../core-helpers';
 
 const formPath = (formSlug, kappSlug) =>
@@ -7,32 +7,44 @@ const formPath = (formSlug, kappSlug) =>
 const submissionPath = submissionId =>
   `${bundle.spaceLocation()}/submissions/${submissionId}`;
 
-export class CoreForm extends Component {
+export class CoreForm extends PureComponent {
   componentDidMount() {
-    const path = this.props.submission
-      ? submissionPath(this.props.submission)
-      : formPath(this.props.form, this.props.kapp);
-    K.load({
-      path: this.props.review ? `${path}?review` : path,
-      container: this.container,
-      loaded: (form) => {
-        this.form = form;
-        if (typeof this.props.loaded === 'function') {
-          this.props.loaded(form);
-        }
-      },
-      unauthorized: this.props.unauthorized,
-      forbidden: this.props.forbidden,
-      notFound: this.props.notFound,
-      error: this.props.error,
-      created: this.props.created,
-      updated: this.props.updated,
-      completed: this.props.completed,
-    });
+    this.loadForm();
+  }
+
+  componentDidUpdate() {
+    this.form.then(form => form.close());
+    this.loadForm();
   }
 
   componentWillUnmount() {
-    this.form.close();
+    this.form.then(form => form.close());
+  }
+
+  loadForm() {
+    const { submission, kapp, form } = this.props;
+    const path = this.submission ? submissionPath(submission) : formPath(form, kapp);
+    this.form = new Promise(
+      (resolve) => {
+        K.load({
+          path: this.props.review ? `${path}?review` : path,
+          container: this.container,
+          loaded: (formData) => {
+            resolve(formData);
+            if (typeof this.props.loaded === 'function') {
+              this.props.loaded(form);
+            }
+          },
+          unauthorized: this.props.unauthorized,
+          forbidden: this.props.forbidden,
+          notFound: this.props.notFound,
+          error: this.props.error,
+          created: this.props.created,
+          updated: this.props.updated,
+          completed: this.props.completed,
+        });
+      },
+    );
   }
 
   render() {
