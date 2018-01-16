7a69c4d17e986c76be1cb8ab407ec2e2637e0567
Shayne Koestler <shayne.koestler@kineticdata.com>
2018-01-16 12:33:24 -0600

    adds react and react-dom as dev dependncies so it can be built after fresh yarn install

--- src/components/utils/loadable.js
+++ src/components/utils/loadable.js
@@ -47,6 +47,7 @@ export const loadable = ({ onMount, onUnmount }) => WrappedComponent =>
     }
 
     componentWillMount() {
+      // eslint-disable-next-line no-console
       console.warn('The "loadable" HOC has been deprecated and will be removed in 1.0.');
       this.mountComponent(this.props);
     }
