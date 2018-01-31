24797d351b040e98566d6497b2d6a81245f0547a
Matt Raykowski <matt.raykowski@kineticdata.com>
2018-01-31 12:39:19 -0600

    KIN-355: Fixed forms CoreAPI to use API path instead of kapp path.

--- src/apis/forms.js
+++ src/apis/forms.js
@@ -10,8 +10,12 @@ import {
 export const fetchForms = (options = {}) => {
   const { kappSlug = bundle.kappSlug(), datastore } = options;
 
+  const path = datastore
+    ? `${bundle.apiLocation()}/datastore/forms`
+    : `${bundle.apiLocation()}/kapps/${kappSlug}/forms`;
+
   // Build URL and fetch the space.
-  let promise = axios.get(formPath({ kapp: kappSlug, datastore }), {
+  let promise = axios.get(path, {
     params: paramBuilder(options),
   });
   // Remove the response envelop and leave us with the forms one.
@@ -31,17 +35,14 @@ export const fetchForm = (options = {}) => {
     throw new Error('fetchForm failed! The option "formSlug" is required.');
   }
 
+  const path = datastore
+    ? `${bundle.apiLocation()}/datastore/forms/${formSlug}`
+    : `${bundle.apiLocation()}/kapps/${kappSlug}/forms/${formSlug}`;
+
   // Build URL and fetch the space.
-  let promise = axios.get(
-    formPath({
-      kapp: kappSlug,
-      form: formSlug,
-      datastore,
-    }),
-    {
-      params: paramBuilder(options),
-    },
-  );
+  let promise = axios.get(path, {
+    params: paramBuilder(options),
+  });
   // Remove the response envelop and leave us with the form one.
   promise = promise.then(response => ({ form: response.data.form }));
   promise = promise.then(deserializeAttributes('attributes', 'form'));
